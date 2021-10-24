dd360fbb04efd218291b900fed4b4ba78d03da19
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2021-10-24 21:44:10 +0300

    added comment to locales.dart described why this file exists

--- lib/locales.dart
+++ lib/locales.dart
@@ -1,1 +1,18 @@
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
+/*
+By default according to https://flutter.dev/docs/development/accessibility-and-localization/internationalization
+Packages:
+- flutter_localization
+- intl
+added to dependecies.
+
+The generated localizations are located by default at: 
+project_folder/.dart_tool/flutter_gen/gen_l10n
+
+and file for import is app_localizations.dart
+
+So finally the url to import is:
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+But, VSCode doesn't suggest to import it, so, this "locales.dart" file exporting the 'package:flutter_gen/gen_l10n/app_localizations.dart' for VSCode to suggest imports for the AppLocalizations class to be able to use.
+*/
