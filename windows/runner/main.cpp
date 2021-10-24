4e30234bc7cb8efd3480609127cab381816d42fd
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2021-10-24 14:55:55 +0300

    renamed window title for windows build

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(100, 50);
   Win32Window::Size size(480, 640);
-  if (!window.CreateAndShow(L"color_palette", origin, size)) {
+  if (!window.CreateAndShow(L"Material Colors Palette", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
