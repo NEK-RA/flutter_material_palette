3cd79a2e01d6c27e76c02bfdd8c9ae151b6f2f65
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2021-11-09 14:08:23 +0300

    version changed to 1.1.0+2

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   // About app
-  static const String appVersion = '1.0.0';
-  static const String appBuild = '1';
+  static const String appVersion = '1.1.0';
+  static const String appBuild = '2';
   static const String homepageUrl = 'https://github.com/NEK-RA/flutter_material_palette';
 }
